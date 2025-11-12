fc6d5f7b51f11fd5740452ce29351719417582e7
gmoinbong <vladosos.scherba@gmail.com>
2025-11-13 01:17:45 +0200

    chore: remove unused files and components to streamline the project structure

--- app/types/index.ts
+++
@@ -1,7 +0,0 @@
-// Domain types will be defined here
-export interface User {
-  id: string;
-  email: string;
-  name?: string;
-}
-
--- app/widgets/index.ts
+++
@@ -1,5 +0,0 @@
-// Widgets public API
-export { ExampleWidget } from './example-widget';
-
-
-
